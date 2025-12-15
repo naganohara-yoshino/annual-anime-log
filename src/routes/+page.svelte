996e4db68aa7341f49dcf7abc36730612fed9e37
naganohara-yoshino <naganohara-yoshino@outlook.com>
2025-12-16 07:00:18 +0800

    feat: Implement annual anime log viewing functionality with new routes and DaisyUI integration.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,11 +54,8 @@ function handleSubmit() {
 							<option value={y}>{y}</option>
 						{/each}
 					</select>
-					<!-- Down arrow icon -->
 					<div class="pointer-events-none absolute right-4 top-1/2 -translate-y-1/2 text-purple-100">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="h-5 w-5">
-							<path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-						</svg>
+						<span class="flex items-center justify-center text-2xl icon-[mi--select]"></span>
 					</div>
 				</div>
 			</div>
