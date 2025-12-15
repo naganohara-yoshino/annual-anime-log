4be43cf7d9bc2184c5a533302954bf3cf9722fb4
naganohara-yoshino <naganohara-yoshino@outlook.com>
2025-12-15 22:43:44 +0800

    chore: clean up

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { resolve } from '$app/paths';
+import { resolve } from "$app/paths";
 </script>
 
 <svelte:head>
@@ -21,10 +21,5 @@
 		The page you're looking at is purely static HTML, with no client-side interactivity needed.
 		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
 		the devtools network panel and reloading.
-	</p>
-
-	<p>
-		The <a href={resolve('/sverdle')}>Sverdle</a> page illustrates SvelteKit's data loading and form
-		handling. Try using it with JavaScript disabled!
 	</p>
 </div>
